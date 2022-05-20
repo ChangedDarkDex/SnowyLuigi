9635d66af68b1ec84aa49dc5a5b9082bf4ca0647
ChangedDarkDex <104589490+ChangedDarkDex@users.noreply.github.com>
2022-05-20 21:46:15 +0200

    Update file.sh

--- file.sh
+++ file.sh
@@ -2,29 +2,18 @@
 pkg update
 pkg install figlet -y
 clear
-echo "..."
-sleep 4
-clear
-figlet Welcome!
-echo "please wait while i do everything you need to do"
-sleep 3
-pkg update
-clear
-echo "..."
-sleep 3
-clear
-echo "Installed!"
-figlet Installed!
-echo "we need to do one last thing before we end the installation"
-sleep 4
-apt install toilet
-echo "Please Wait..."
-sleep 5
-clear
-toilet Done!
-figlet Done!
-echo "Done!"
-echo "Hope you enjoy our program. Closing this down..."
-sleep 6
-clear
+PS3='do you want to install? : '
+options=("yes" "no")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "yes")
+            echo "you chose choice 1"
+            ;;
+        "no")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
 
