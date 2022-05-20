150fcf3d29382a1785fc3b73868a6de48b34997d
ChangedDarkDex <104589490+ChangedDarkDex@users.noreply.github.com>
2022-05-20 10:44:17 +0200

    Create file2.sh

---
+++ file2.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+pkg install figlet -y
+clear
+echo "..."
+sleep 4
+clear
+figlet Welcome!
+echo "please wait while i do everything you need to do"
+pkg update
+clear
+echo "!.!."
+sleep 3
+clear
+echo "Installed!
+figlet Installed!
